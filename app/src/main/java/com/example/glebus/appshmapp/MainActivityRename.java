0083cb4e4cc4ce577b8fb97b55cdfa9b6ce21978
Glebus <zaborovskij.gleb@gmail.com>
2018-11-26 01:36:52 +0300

    initial commit

---
+++ app/src/main/java/com/example/glebus/appshmapp/MainActivityRename.java
@@ -0,0 +1,13 @@
+package com.example.glebus.appshmapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityRename extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_rename);
+    }
+}
